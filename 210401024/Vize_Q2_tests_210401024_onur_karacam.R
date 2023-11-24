4a0e32e70226c4dc7e8c6fe542c1162cb117c691
onurkaracam <onur.karacam006@gmail.com>
2023-11-24 16:52:31 +0300

    2.1, testleri ile tamamlandı.

---
+++ 210401024/Vize_Q2_tests_210401024_onur_karacam.R
@@ -0,0 +1,12 @@
+library(testthat)
+
+current_dir <- getwd()
+print(current_dir)
+relative_path <- file.path(current_dir, "Vize_Q2_210401024_onur_karacam.R")
+
+source(relative_path)
+
+# Test 2.1
+test_that("Global Workspace 'spotify_search_artist' degiskenine sahip mi", {
+  expect_true(exists("spotify_search_artist", envir = .GlobalEnv))
+})
